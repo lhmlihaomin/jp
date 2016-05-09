3f3aee102983884c834761d4d5d27bb220f4be1b
lhmlihaomin <lhmlihaomin@live.com>
2016-05-09 23:59:00 +0800

    added static files

--- notebook/urls.py
+++ notebook/urls.py
@@ -3,7 +3,8 @@ from notebook import views
 
 urlpatterns = [
     url(r'v1/', views.v1, name='v1'),
-    url(r'lessons/', views.lessons, name="lessons"),
-    url(r'^words/', views.words, name="words"),
-    url(r'^sentences/', views.sentences, name="sentences"),
+    url(r'lessons/$', views.lessons, name="lessons"),
+    url(r'lessons/add/', views.lessons_add, name="lessons_add"),
+    url(r'^words/([0-9]+)/', views.words, name="words"),
+    url(r'^sentences/([0-9]+)/', views.sentences, name="sentences"),
 ]
--- notebook/views.py
+++ notebook/views.py
@@ -1,26 +1,59 @@
+import re
+
 from django.shortcuts import render
-from django.http import HttpRequest, HttpResponse, HttpResponseRedirect 
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
+ 
 from notebook.models import Lesson, Word, Sentence
 
 # Create your views here.
 def lessons(request):
     """Show a list of all lessons"""
-    lessons = Lesson.objects.all()
+    # get and sort all lessons:
+    lessons = Lesson.objects.order_by("name")
+    # increment the last lesson name by "1" as the new default name:
+    if len(lessons) > 0:
+        last_lesson_name = lessons[len(lessons)-1].name
+        p = "([a-zA-Z]+)(\d+)"
+        m = re.match(p, last_lesson_name)
+        if m is not None:
+            order_len = len(m.groups()[1])
+            new_order = int(m.groups()[1]) + 1
+            fmt = "%s%0"+str(order_len)+"d"
+            new_name = fmt%(m.groups()[0], new_order)
+        else:
+            new_name = "A01"
     print(len(lessons))
-    return HttpResponse([x.name for x in lessons])
+    return render(request, "notebook/lessons.html", locals())
     
-def words(request):
+def lessons_add(request):
+    """Add a lesson."""
+    name = request.POST.get("new_lesson_name").strip()
+    note = request.POST.get("new_lesson_note").strip()
+    print("Name: ", repr(name))
+    print("Note: ", repr(note))
+    p = "([a-zA-Z]+)(\d+)"
+    m = re.match(p, name)
+    if len(name) == 0 or m is None:
+        return HttpResponse("Invalid lesson name.")
+    lesson = Lesson.objects.create(
+        name = name,
+        note = note
+    )
+    return HttpResponseRedirect(reverse("lessons"))
+    
+def words(request, index):
     """Show a list of all words in a lesson"""
-    lesson = Lesson.objects.get(pk=request.GET.get("lesson"))
+    lesson = Lesson.objects.get(pk=int(index))
     #words = Word.objects.get(lesson=lesson)
     #words = Lesson.word_set.all()
     words = Word.objects.filter(lesson=lesson)
     
     return HttpResponse([x.word for x in words])
     
-def sentences(request):
+def sentences(request, index):
     """Show a list of all sentences in a lesson"""
-    lesson = Lesson.objects.get(pk=request.GET.get("lesson"))
+    lesson = Lesson.objects.get(pk=int(index))
     #sentences = lesson.sentence_set.all()
     sentences = Sentence.objects.filter(lesson=lesson)
     return HttpResponse([x.sentence for x in sentences])
