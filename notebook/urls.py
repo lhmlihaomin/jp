005ec4054410628a8bc5ef8af3a40ac20f12044c
lhmlihaomin <lhmlihaomin@live.com>
2016-05-12 23:00:29 +0800

    1. redesigned url patterns; 2. changed navbar style.

--- notebook/urls.py
+++ notebook/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     url(r'v1/', views.v1, name='v1'),
     url(r'lessons/$', views.lessons, name="lessons"),
     url(r'lessons/add/', views.lessons_add, name="lessons_add"),
-    url(r'^words/([0-9]+)/', views.words, name="words"),
-    url(r'^sentences/([0-9]+)/', views.sentences, name="sentences"),
+    url(r'^lessons/([0-9]+)/words/', views.words, name="words"),
+    url(r'^lessons/([0-9]+)/sentences', views.sentences, name="sentences"),
 ]
--- notebook/views.py
+++ notebook/views.py
@@ -1,3 +1,4 @@
+# coding=utf8
 import re
 
 from django.shortcuts import render
