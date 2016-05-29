c9b5451e9c896c5bef7f6fc979f70a08231eeccc
lhmlihaomin <lhmlihaomin@live.com>
2016-05-29 22:46:56 +0800

    reassigned url

--- jp/urls.py
+++ jp/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^notebook/', include("notebook.urls")),
+    url(r'^', include("notebook.urls")),
 ]
